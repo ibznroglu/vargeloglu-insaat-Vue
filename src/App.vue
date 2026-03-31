eaa05aea3a476f4ae752cb2d0d8a8dd2ebabf9b7
ibznrMobile <ibznroglu@gmail.com>
2026-04-01 01:50:23 +0300

    refactor(components): remove unused components and update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <MainLayout>
     <HeroSlider />
-    <HomeQuickOverview />
-    <About />
     <Services />
     <Projects />
     <Contact />
@@ -12,8 +10,6 @@
 <script setup lang="ts">
 import MainLayout from './layouts/MainLayout.vue'
 import HeroSlider from './components/HeroSlider.vue'
-import HomeQuickOverview from './components/HomeQuickOverview.vue'
-import About from './components/About.vue'
 import Services from './components/Services.vue'
 import Projects from './components/Projects.vue'
 import Contact from './components/Contact.vue'
